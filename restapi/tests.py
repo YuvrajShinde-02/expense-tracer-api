4a46c9c59b9235f7b9f8fad9e807bc6762284c56
rickgrims <yuvrajjshinde@gmail.com>
2022-01-17 12:25:09 +0530

    installed django-rest-framework

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,11 +1,17 @@
-# from django.test import TestCase
-from unittest import TestCase
+from django.test import TestCase
+from restapi import models
 
-# Create your tests here.
-def two_integers_sum(a, b):
-    return a + b
-
-
-class TestSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(two_integers_sum(1, 2), 3)
+# Create your tests here
+class TestModel(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            descreption="anc headphones",
+            category="music",
+        )
+        insertes_expense = models.Expense.objects.get(pk=expense.id)
+        self.assertEqual(249.99, insertes_expense.amount)
+        self.assertEqual("amazon", insertes_expense.merchant)
+        self.assertEqual("anc headphones", insertes_expense.descreption)
+        self.assertEqual("music", insertes_expense.category)
